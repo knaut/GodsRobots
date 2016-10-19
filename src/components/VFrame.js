fd766bdbe107090c70edf50cdf1b7da64c43fb74
_knaut <info@knaut.net>
2016-10-19 00:26:01 -0700

    removed video bg for speed issues

--- src/components/VFrame.js
+++ src/components/VFrame.js
@@ -4,9 +4,10 @@ var VFrame = Ulna.Component.extend({
 	root: '#vframe',
 
 	template: {
-		'video#vframe-video[autoplay=""][muted][poster="/media/knautilus_horiz_logo_solid.png][loop=""]': {
-			'source[src="/media/videos/burnItUp.mp4"][type="video/mp4"]':'',
-		}
+		'#vframe-video': ''
+		// 'video#vframe-video[autoplay=""][muted][poster="/media/knautilus_horiz_logo_solid.png][loop=""]': {
+		// 	'source[src="/media/videos/burnItUp.mp4"][type="video/mp4"]':'',
+		// }
 	}
 });
 
